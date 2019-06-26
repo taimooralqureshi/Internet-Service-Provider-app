fc7b8b90c4f3a541c14f7f8719478b2a0436614a
WajeehaParker <wajeeha.parker@gmail.com>
2019-06-26 21:12:27 +0500

    changes in add and display

--- isp_db.sql
+++ isp_db.sql
@@ -482,11 +482,22 @@ CREATE TABLE fa_Entry (
 );
 
 insert into fa_Transaction (id, date, trans_type) values
-(1, "2019/05/15", "Normal");
+(1, "2019/05/15", "Normal"),
+(2, "2019/05/15", "Adjustment"),
+(3, "2019/05/15", "Normal"),
+(4, "2019/05/15", "Adjustment");
+
+select * from fa_Transaction;
 
 insert into fa_Entry (type,amount,account_name,trans_id) values
 ("Debit", 1000, "Cash", 1),
-("Credit", 1000, "OC", 1);
+("Credit", 1000, "OC", 1),
+("Debit", 2000, "AP", 2),
+("Credit", 2000, "Cash", 2),
+("Debit", 1000, "Cash", 3),
+("Credit", 1000, "OC", 3),
+("Debit", 2000, "AP", 4),
+("Credit", 2000, "Cash", 4);
 
 select * from fa_Entry;
 
@@ -507,7 +518,7 @@ FROM
     fa_Transaction t ON e.trans_id = t.id
         LEFT OUTER JOIN
     fa_Account a ON e.account_name = a.name
-ORDER BY e.type, t.id ;
+ORDER BY t.id, t.id ;
 
 END$$ 
 DELIMITER ;
@@ -531,7 +542,8 @@ FROM
     fa_Transaction t ON e.trans_id = t.id
         LEFT OUTER JOIN
     fa_Account a ON e.account_name = a.name
-	where e.id = id;
+	where e.id = id
+ORDER BY t.id, t.id ;
 		
 END$$ 
 DELIMITER ;
